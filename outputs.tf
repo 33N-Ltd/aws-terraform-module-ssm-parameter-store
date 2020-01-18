a2fa936c7d2692711f770b1815bc4761e8da8572
Sion Smith <sion.smith@gmail.com>
2020-01-18 12:16:29 +0000

    fix around conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -7,38 +7,32 @@ locals {
 }
 
 output "names" {
-  count = length(local.name_list) > 0 ? 1 : 0
-  value       = local.name_list
+  value       = join("", local.name_list)
   description = "A list of all of the parameter names"
 }
 
 output "values" {
-  count = length(local.value_list) > 0 ? 1 : 0
   description = "A list of all of the parameter values"
-  value       = local.value_list
+  value       = join("", local.value_list)
 }
 
 output "map" {
-  count = length(local.name_list) && length(local.value_list) > 0 ? 1 : 0
   description = "A map of the names and values created"
-  value       = zipmap(local.name_list, local.value_list)
+  value       = join("", zipmap(local.name_list, local.value_list))
 }
 
 output "map_names" {
-  count = length(local.name_list_map) > 0 ? 1 : 0
   description = "A list of all of the parameter names"
-  value       = local.name_list_map
+  value       =  join("", local.name_list_map)
 }
 
 output "map_values" {
-  count = length(local.value_list_map) > 0 ? 1 : 0
-  value       = local.value_list_map
+  value       = join("", local.value_list_map)
   description = "A list of all of the parameter names"
 }
 
 output "kv" {
-  count = length(var.read_parameter_map) && length(local.name_list) && length(local.value_list_map) &&  length(local.value_list)  > 0 ? 1 : 0
   description = "A map of the names and values created"
-  value       = zipmap(coalescelist(keys(var.read_parameter_map), local.name_list),
-                          coalescelist(local.value_list_map, local.value_list))
+  value       = join("", zipmap(coalescelist(keys(var.read_parameter_map), local.name_list),
+                          coalescelist(local.value_list_map, local.value_list)))
 }
